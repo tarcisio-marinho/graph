674c3201973db3c706ce1e02b8cbfe6bfab140a8
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-16 20:37:31 -0300

    up

--- graph.py
+++ graph.py
@@ -1,26 +1,32 @@
 
 import Queue
 
-
-def create_graph(graph, elements):
-    for e in range(elements):
-        node = raw_input("Node> ")
-        conn = raw_input("connects: ")
-        conn = conn.split(" ")
-        if(not node in graph):
-            graph[node] = conn
+class Graph():
+    def __init__(n):
+        self.g = {}
+        for i in range(n):
+            node = raw_input("Node: ")
+            conn = raw_input("Destinations: ")
+            conn = conn.split()
+            if(not node in g):
+                g[node] = conn
         
-def dfs(graph):
-    q = Queue.Queue()
-    for node in graph:
-        for element in graph.get(node):
-            q.put(element) # insert each path in que queue
+    def dfs(self, graph):
+        q = Queue.Queue()
+        for node in graph:
+            for element in graph.get(node):
+                q.put(element) # insert each path in que queue
+
+    def edges(self):
+        edges = []
+        for node in g:
+            for dest in g[node]:
+                edges.append((node, dest))
+        return edges
 
 
 if __name__ == "__main__":
     number = int(raw_input("Number of nodes: "))
-    graph = {}
-
-    create_graph(graph, number)
-    dfs(graph)
+    graph = Graph(number)
+    print graph.edges()
 
