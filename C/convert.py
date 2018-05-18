7b8f548f337d9827f5a862d8f57b33358c89fa5a
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-05-18 11:53:02 -0300

    python parser graph

--- C/convert.py
+++ C/convert.py
@@ -3,6 +3,23 @@ file1 = "grafo2.g"
 output = "grafo.graphml"
 
 
+start_file = """<?xml version="1.0" encoding="UTF-8"?>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns"  
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+     http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+  <graph id="G" edgedefault="directed">\n"""
+
+end_file = """  </graph>
+</graphml>"""
+
+new_node = ""
+
+node_declaration = '    <node id="{}"/>\n'
+
+edge_declaration = '    <edge source="{}" target="{}"/>\n'
+
+
 with open(file1) as f:
     grafo1 = f.read()
 
@@ -10,14 +27,25 @@ grafo1 = grafo1.split("\n")
 grafo1.remove('1') # start of the algorithm
 grafo1.remove("13 21") # end of the algorithm and number of arestas
 
+complete_file = ""
+complete_file +=start_file
+
+i = 1
+while(i < 14):
+    complete_file += node_declaration.format(i)
+    i += 1
+
+
 for conn in grafo1:
     nodes = conn.split()
     source = nodes[0]
     destination = nodes[1]
     weight = nodes[2]
+    complete_file += edge_declaration.format(source, destination)
 
-    print(source, destination, weight)
-
-print(grafo1)
+complete_file += end_file
 
 
+
+
+print(complete_file)
